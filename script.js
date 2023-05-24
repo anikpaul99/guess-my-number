7e91d85fc274f8bc5b32428cbab33760ff09f883
Anik Paul <132483203+anikpaul99@users.noreply.github.com>
2023-05-24 21:46:21 +0600

    fix: secret number showing at the beginning
    secret number will be only showed when the user guess the correct number

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -13,6 +12,8 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
